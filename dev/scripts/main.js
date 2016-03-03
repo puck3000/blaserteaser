ec49365487901d07cb4f04e16d56b4682b795962
puck3000 <daniel.lerch@gmail.com>
2016-03-03 12:06:38 +0100

    adding accordeon

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,22 @@
 $(function() {
    console.log('hello world!');
+
+
+
+/*
+ **********************
+* Countdown
+ **********************
+*/
+
+$("#Countdown")
+   .countdown("2016/10/01", function(event) {
+     $(this).text(
+       event.strftime('%D Tage %H Stunden %M Minuten %S Sekunden')
+     );
+   });
+
+
+
+
 });
